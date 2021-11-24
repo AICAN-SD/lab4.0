de631490dfb219c86e4db5ae9f9a8254962d0f5b
Alankrit Arya <alankritarya15@gmail.com>
2021-11-24 20:15:04 +0530

    sidebar done

--- js/main.js
+++ js/main.js
@@ -25,15 +25,41 @@ function includeHTML() {
       }
     }
   }
-  function hello(){
-    var ele = document.getElementById("link3")
-    var x = document.getElementById("link1")
-    var elep = document.getElementById("sit-amet")
-    var xp = document.getElementById("lorem")
-    ele.classList.add("active")
-    x.classList.remove("active")
-    elep.classList.add("show")
-    elep.classList.add("active")
-    xp.classList.remove("show")
-    xp.classList.remove("active")
+
+  var device = document.getElementById("link1")
+  var settings = document.getElementById("link2")
+  var setups = document.getElementById("link3")
+
+  var device_c = document.getElementById("device_c")
+  var setting_c = document.getElementById("setting_c")
+  var setup_c = document.getElementById("setup_c")
+
+function devices(){
+  device.classList.add("active")
+  settings.classList.remove("active")
+  setups.classList.remove("active")
+
+  device_c.classList.add("show","active")
+  setting_c.classList.remove("show","active")
+  setup_c.classList.remove("show","active")
+}
+
+function setting(){
+  device.classList.remove("active")
+  settings.classList.add("active")
+  setups.classList.remove("active")
+
+  device_c.classList.remove("show","active")
+  setting_c.classList.add("show","active")
+  setup_c.classList.remove("show","active")
+}
+
+function setup(){
+  device.classList.remove("active")
+  settings.classList.remove("active")
+  setups.classList.add("active")
+
+  device_c.classList.remove("show","active")
+  setting_c.classList.remove("show","active")
+  setup_c.classList.add("show","active")
 }
