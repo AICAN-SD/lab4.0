cf90de4a27a02a568df75fe29330c14ef7d513be
Alankrit Arya <alankritarya15@gmail.com>
2021-11-28 18:20:47 +0530

    final commit

--- js/pump_job.js
+++ js/pump_job.js
@@ -40,3 +40,9 @@ function includeHTML() {
         pump_timer.style.display = "none";
     }
   }
+  function toggle(){
+    let elm = document.getElementById('mytoggle');
+    let elm2 = document.getElementById('mytoggle2');
+    elm.classList.toggle("active")
+    elm2.classList.toggle("active")
+}
